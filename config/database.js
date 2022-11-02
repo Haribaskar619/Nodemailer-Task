4b5d263891111ea7e770b86a0f9300611db39a0f
Haribaskar619 <hariganesh1795@gmail.com>
2022-11-02 11:10:52 +0530

    removed console on database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    console.log(process.env.MONGO_URI)
+   
 try {
     const connection = await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
